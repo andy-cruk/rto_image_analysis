956bba9293d9f135a772a2639ce848190d5bea78
Peter Smittenaar <petersmittenaar+github@gmail.com>
2015-10-02 15:41:44 +0100

    +load all classifications into dataframe and store it +timekeeping through loops

--- user_aggregation.py
+++ user_aggregation.py
@@ -23,20 +23,30 @@ print 'Number of subjects:\t\t\t',subjectsCollection.count()
 print 'Number of classifications:\t',classifCollection.count()
 
 ####### run over each classification and store its properties in a pandas dataframe
-# .find() selects the actual entries so you can loop over them, index them, and so on
-sel = classifCollection.find()
-# set up dataframe with required columns
-next up: understand how to set the index in DataFrame to the name of the segment through clever retrieval of subject_ids http://docs.mongodb.org/manual/core/cursors/
-scores = pd.DataFrame(columns=["segment ID","cancer","proportion stained","staining intensity"])
-# loop over each entry in rawdat
+# set up dataframe with indices (row names) equal to the identifier of the segment
+segments = list()
+for cl in classifCollection.find().limit():
+    segments.append(cl["subject_ids"][0])
+
+# initialise 'scores' dataframe with columns and rows. If you don't initialise with this number of rows, it's unbearably slow to fill the rows 1 by 1 in the loop later.
+scores = pd.DataFrame(columns=["cancer","proportion stained","staining intensity"],index=segments,dtype=int)
+
+# # keep track of time for entire loop
 startTime = time.time()
-for iC in range(sel.count()):
-# for iC in range(100):
-    scores.loc[iC,"segment ID"] = sel[iC]["subject_ids"][0]                     # store segment ID
-    scores.loc[iC,"cancer"] = sel[iC]["annotations"][0]["a-1"]                  # store answer to Cancer yes/no
-    scores.loc[iC,"proportion stained"] = sel[iC]["annotations"][1]["a-2"]      # store answer to proportion stained
-    scores.loc[iC,"staining intensity"] = sel[iC]["annotations"][2]["a-3"]      # store answer to intensity of staining
-    print iC
+# # keep track of time inside the loop
+t=np.empty([1])
+t[0]=time.time()
+# loop over all requested classifications
+ix = 0 #index in 'scores'
+for cl in classifCollection.find().limit(): # go through all samples
+    scores.iloc[ix,0] = cl["annotations"][0]["a-1"]          # store answer to Cancer yes/no
+    scores.iloc[ix,1] = cl["annotations"][1]["a-2"]      # store answer to proportion stained
+    scores.iloc[ix,2] = cl["annotations"][2]["a-3"]      # store answer to intensity of staining
+    ix+=1
+    if ix%10000==0:
+        print ix,"Last 10k samples took",time.time()-t[0],"seconds"
+        t[0]=time.time()
+
 # transform variables to integers rather than objects (compare scores.dtypes with scores.convert_objects(convert_numeric=True).dtypes). This is because setting the dtype during initialisation of "scores" does not work
 scores = scores.convert_objects(convert_numeric=True)
 print "Time passed:",time.time()-startTime,"seconds"
