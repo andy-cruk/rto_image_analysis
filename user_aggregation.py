482fe9f34cf4f36da2db37d1a67af38c03e3db5b
Peter Smittenaar <petersmittenaar+github@gmail.com>
2015-10-02 18:02:56 +0100

    stores panda dataframe or loads if exists; added example functions on how to work with dataframe

--- user_aggregation.py
+++ user_aggregation.py
@@ -3,7 +3,10 @@
 # options
 dataset = "TEST MRE11"          #what dataset to look at
 minClassifications = 1          #inclusive lower bound number of classifications per segment in order to include that segment
+maxClassifications = 10^10       #max classifications to retrieve; mostly for debugging when you want the script to be fast
 
+# constants
+classificationsDataframeFn = "classifications_dataframe.pkl"    # will store the pandas dataframe created from all the classifications; file will load if the file exists already to prevent another 30 to 60 min of processing.
 
 
 __author__ = 'Peter'
@@ -13,48 +16,63 @@ import pandas as pd
 import scipy as s
 import time
 import numpy as np
+import os.path
 
 # open connection to local data
 dbConnection = MongoClient("localhost", 27017)
 subjectsCollection = dbConnection.RTO_20150929.subjects
 classifCollection = dbConnection.RTO_20150929.classifications
+nSj = subjectsCollection.count()
+nCl = classifCollection.count()
 # show that subjects_collection is a pymongo type variable
-print 'Number of subjects:\t\t\t',subjectsCollection.count()
-print 'Number of classifications:\t',classifCollection.count()
+print 'Number of subjects:\t\t\t',nSj
+print 'Number of classifications:\t',nCl
 
-####### run over each classification and store its properties in a pandas dataframe
-# set up dataframe with indices (row names) equal to the identifier of the segment
-segments = list()
-for cl in classifCollection.find().limit():
-    segments.append(cl["subject_ids"][0])
+####### check if dataframe with classifications exists; if not, run over each classification and store its properties in a pandas dataframe. If it does, load it.
+if os.path.isfile(classificationsDataframeFn)==False:
+    print "No saved .pkl with dataframe for classifications detected, putting it together now. Could take 30+ mins..."
+    # collect a list of the segment identifier names for each classification. This will be used as index (row name) later
+    segments = list()
+    for cl in classifCollection.find().limit(maxClassifications):
+        segments.append(cl["subject_ids"][0])
 
-# initialise 'scores' dataframe with columns and rows. If you don't initialise with this number of rows, it's unbearably slow to fill the rows 1 by 1 in the loop later.
-scores = pd.DataFrame(columns=["cancer","proportion stained","staining intensity"],index=segments,dtype=int)
+    # initialise 'scores' dataframe with columns and rows. If you don't initialise with this number of rows, it's unbearably slow to fill the rows 1 by 1 in the loop later.
+    scores = pd.DataFrame(columns=["cancer","proportion stained","staining intensity"],index=segments)
 
-# # keep track of time for entire loop
-startTime = time.time()
-# # keep track of time inside the loop
-t=np.empty([1])
-t[0]=time.time()
-# loop over all requested classifications
-ix = 0 #index in 'scores'
-for cl in classifCollection.find().limit(): # go through all samples
-    scores.iloc[ix,0] = cl["annotations"][0]["a-1"]          # store answer to Cancer yes/no
-    scores.iloc[ix,1] = cl["annotations"][1]["a-2"]      # store answer to proportion stained
-    scores.iloc[ix,2] = cl["annotations"][2]["a-3"]      # store answer to intensity of staining
-    ix+=1
-    if ix%10000==0:
-        print ix,"Last 10k samples took",time.time()-t[0],"seconds"
-        t[0]=time.time()
+    # loop over all requested classifications
+    ix = 0 #index in 'scores'
+    for cl in classifCollection.find().limit(maxClassifications): # go through all samples
+        scores.iloc[ix,0] = cl["annotations"][0]["a-1"]      # store answer to Cancer yes/no
+        scores.iloc[ix,1] = cl["annotations"][1]["a-2"]      # store answer to proportion stained
+        scores.iloc[ix,2] = cl["annotations"][2]["a-3"]      # store answer to intensity of staining
+        ix+=1
+        if ix%10000==0:
+            print "Samples processed:\t",100*ix/nCl,"%"
 
-# transform variables to integers rather than objects (compare scores.dtypes with scores.convert_objects(convert_numeric=True).dtypes). This is because setting the dtype during initialisation of "scores" does not work
-scores = scores.convert_objects(convert_numeric=True)
-print "Time passed:",time.time()-startTime,"seconds"
-# save the dataframe; load using x=pd.read_pickle(file_name)
-scores.to_pickle("classifications_dataframe.pkl")
+    # transform variables to integers rather than objects (compare scores.dtypes with scores.convert_objects(convert_numeric=True).dtypes). This is because setting the dtype during initialisation of "scores" does not work
+    scores = scores.convert_objects(convert_numeric=True)
+
+    # save the dataframe; load using x=pd.read_pickle(classificationsDataframeFn)
+    scores.to_pickle(classificationsDataframeFn)
+    print "done saving dataframe"
+else:
+    print ".pkl with classifications in dataframe found; loading now..."
+    scores = pd.read_pickle(classificationsDataframeFn)
+    print "... done loading dataframe"
+
+####### here are some example commands of how to work with the dataframe
+# # select only classifications that said 'yes' to cancer and return all columns
+# scores[scores['cancer']==1]
+# # select only classifications with cancer and return second and third column
+# scores.loc[scores['cancer']==1,'proportion stained':'staining intensity']
+# # plot histogram of scores across columns (combine with previous to get histogram for cancer only)
+# scores.hist()
+# # combine selection criteria, e.g. to check for unallowed score combinations
+# scores.loc[(scores['cancer']==2) & (scores['proportion stained']!=0),'proportion stained':'staining intensity'].hist(bins=20)
+# # view data type of columns
+# scores.dtypes
 
 
-
-
-# close the connection to local mongoDB
+####### close the connection to local mongoDB
 dbConnection.close()
+print "All done with script"
