6d6bae6e43e61a8d6ae18722b79102a341245cbc
Peter Smittenaar <petersmittenaar+github@gmail.com>
2015-10-14 15:19:47 +0100

    rewrote code almost entirely; can now fill a dataframe with one subject per row, and all classifications of that subject aggregated in columns. Requires subject_ids in classifications database to be indexed to be efficient.

--- user_aggregation.py
+++ user_aggregation.py
@@ -8,13 +8,18 @@ import time
 import numpy as np
 import os.path
 
-# options
-stain = ""  # what stain to look at
-minClassificationsPerSegment = 1  # inclusive lower bound number of classifications per segment in order to include that segment
-maxClassifications = 10 ^ 10  # max classifications to retrieve; mostly for debugging when you want the script to be fast
+# user options
+stain = "TEST MRE11"                 # what sample to look at; must match metadata.stain_type in subjects database, TEST MRE11
+minClassifications = 1          # min number of classifications the segment needs to have
 
-# constants
-classificationsDataframeFn = "classifications_dataframe.pkl"  # will store the pandas dataframe created from all the classifications; file will load if the file exists already to prevent another 30 to 60 min of processing.
+# set dictionary with filters to feed to
+filterSubjects = {"$and":[
+    {"metadata.stain_type":stain},
+    {"classification_count":{"$gt":minClassifications}},
+    ]}
+
+# save file for scores
+classificationsDataframeFn = "classifications_dataframe_"+stain+".pkl"  # will store the pandas dataframe created from all the classifications; file will load if the file exists already to prevent another 30 to 60 min of processing.
 
 ########### FUNCTION DEFINITIONS
 def pymongo_connection_open():
@@ -26,67 +31,71 @@ def pymongo_connection_open():
     nCl = classifCollection.count()
     print 'Number of subjects:\t\t\t', nSj
     print 'Number of classifications:\t', nCl
-    return subjectsCollection,classifCollection,nSj,nCl
+    return subjectsCollection,classifCollection,dbConnection
 def pymongo_connection_close():
     dbConnection.close()
-def classifications_dataframe_fill_and_save_one_by_one(maxClassifications=maxClassifications,fn=classificationsDataframeFn):
-    # Pandas dataframe and fill with each classification, including what core it came from and what cancer
-    print "Putting together dataframe. Could take 30+ mins..."
-    # collect a list of the segment identifier names for each classification. This will be used as index (row name) later.
-    # Note that these segments are printed as an alphanumeric string, but the underlying string includes ObjectId('alphanumericstring')
-    segments = list()
-    for cl in classifCollection.find().limit(maxClassifications):
-        segments.append(cl["subject_ids"][0])
-
-    # initialise 'scores' dataframe with columns and rows. If you don't initialise with this number of rows, it's unbearably slow to fill the rows 1 by 1 in the loop later.
-    scores = pd.DataFrame(columns=["cancer", "proportion stained", "staining intensity","core","stain"], index=segments)
-
-    # loop over all requested classifications and fill the entire dataframe
-    ix = 0  # index in 'scores'
-    t = time.time()
-    for cl in classifCollection.find().limit(maxClassifications):  # go through all samples
-        scores.iloc[ix, 0] = cl["annotations"][0]["a-1"]  # store answer to Cancer yes/no
-        scores.iloc[ix, 1] = cl["annotations"][1]["a-2"]  # store answer to proportion stained
-        scores.iloc[ix, 2] = cl["annotations"][2]["a-3"]  # intensity stained
-        # add what core this classification came from and what stain type by accessing the subjectsCollection
-        # 1 or more entries are missing the key 'stain_type' so only finding ones that have it; otherwise it'll return None and the if statement later will catch it
-        sjCursor = subjectsCollection.find_one({'$and': [{'_id':segments[ix]},{["metadata"]["stain_type"]:{"$exists":True}}]})
-        # check if a matching subject was found for this segment - not always the case for some reason
-        if sjCursor is None:
-            print "no matching subject found for core with index",ix,", or it didn't have the 'stain_type' key"
-            scores.iloc[ix,3] = np.NaN
-            scores.iloc[ix,4] = np.NaN
-            ix+=1
-            continue
-        # all should be ok to read out the segment ID
-        # scores.iloc[ix,3] = sjCursor["group_id"] # this is excruciatingly slow; takes about 400 ms per entry, probably because it is an ObjectId
-        scores.iloc[ix,3] = str(sjCursor["group_id"]) # this is much faster because now it has to put in a string rather than ObjectId.
-        scores.iloc[ix,4] = sjCursor["metadata"]["stain_type"]
-        # update counter and report
-        ix += 1
-        if ix%20000  == 0:
-            print "Classifications processed:\t",100*ix/nCl,"%, time remaining:",(1-ix/nCl)*((time.time()-t)/60)/(ix/nCl),"minutes"
-
-    # transform variables to integers rather than objects (compare scores.dtypes with scores.convert_objects(convert_numeric=True).dtypes). This is because setting the dtype during initialisation of "scores" does not work
-    scores = scores.convert_objects(convert_numeric=True)
-
-    # save the dataframe; load using x=pd.read_pickle(classificationsDataframeFn)
-    scores.to_pickle(fn)
-    print "done saving dataframe"
-    return scores
+def classifications_dataframe_fill_and_save(fn=classificationsDataframeFn):
+    # first grab all the subjects that satisfy the filterSubjects criteria and store classification_count and the core it belongs to (group_id)
+    subjectCursor = subjectsCollection.find(filter=filterSubjects,projection=("classification_count","group_id"))
+    assert subjectCursor.count()>0,"no records found for filter"
+    col = ("subjectID","nClassifications","nCancer","noStain","<25%Stain","<50%Stain","<75%Stain","<95%Stain","<100%Stain","stainWeak","stainMedium","stainStrong","errorProp","errorIntensity")
+    # set up dataframe that will store one row per segment, with classification scores. It will count occurrences in each of the buckets, which can later be divided by number of classifications
+    cl = pd.DataFrame(data=None,columns=col)
+    clIx = 0 # dataframe index
+    ts=time.time()
+    for sj in subjectCursor: # loop over each entry in subjectCursor and aggregate ratings into dataframe
+        # store the subject id in the pandas dataframe, initialising the row
+        cl.loc[clIx,"subjectID"] = sj["_id"]
+        # initialise the rest of the row with zeros
+        cl.iloc[clIx,1:] = 0
+        # collect all classifications from this particular segment
+        clCursor = classifCollection.find(filter={"subject_ids":[sj["_id"]]},projection=("annotations",))
+        for iCl in clCursor: # loop over each classification that was retrieved from database
+            cl.loc[clIx,"nClassifications"]+=1
+            # cancer yes/no store in dataframe
+            cancer = int(iCl["annotations"][0]["a-1"])
+            if cancer == 1: # if yes cancer
+                cl.loc[clIx,"nCancer"]+=int(iCl["annotations"][0]["a-1"])
+                propStain = int(iCl["annotations"][1]["a-2"])
+                if propStain == 1:
+                    cl.loc[clIx,"noStain"]+=1
+                elif propStain == 2:
+                    cl.loc[clIx,"<25%Stain"]+=1
+                elif propStain == 3:
+                    cl.loc[clIx,"<50%Stain"]+=1
+                elif propStain == 4:
+                    cl.loc[clIx,"<75%Stain"]+=1
+                elif propStain == 5:
+                    cl.loc[clIx,"<95%Stain"]+=1
+                elif propStain == 6:
+                    cl.loc[clIx,"<100%Stain"]+=1
+                else: # should always be one of the above
+                    cl.loc[clIx,"errorProp"]+=1
+                # now do intensity
+                intensityStain = int(iCl["annotations"][2]["a-3"])
+                if intensityStain == 1:
+                    cl.loc[clIx,"stainWeak"]+=1
+                elif intensityStain == 2:
+                    cl.loc[clIx,"stainMedium"]+=1
+                elif intensityStain == 3:
+                    cl.loc[clIx,"stainStrong"]+=1
+                else: #should always be one of the above
+                    cl.loc[clIx,"errorIntensity"]+=1
+            elif cancer == 2: #if no cancer
+                continue
+        clIx +=1
+        if clIx%500==0:
+            print 100*clIx/subjectCursor.count(),"%"
+    # convert numbers to int (otherwise they're stored as object)
+    cl = cl.convert_objects(convert_numeric=True)
+    print "Saving dataframe to",fn
+    cl.to_pickle(fn)
+    print "Done aggregating dataframe with classifications and saving"
+    return cl
 def classifications_dataframe_load(fn=classificationsDataframeFn):
     # load dataframe and return scores
-    scores = pd.read_pickle(fn)
-    assert len(scores)==classifCollection.find().count(),"number of scores in dataframe does not match number of items in MongoDB"
-    return scores
-def filter_classifications(scores,stain=None,minClassificationsPerSegment=None):
-    # filter the data based on several criteria
-    if stain is not None: # only filter if it is not None
-        scores = scores[scores["stain"]==stain]
-    if minClassificationsPerSegment is not None:
-        segments = scores.index #get all segments
-
-    return scores
+    cl = pd.read_pickle(fn)
+    return cl
 def plot_classifications(scores):
     ###### here are some example commands of how to work with the dataframe
     # select only classifications that said 'yes' to cancer and   return all columns
@@ -105,14 +114,13 @@ def plot_classifications(scores):
 
 ########### FUNCTION EXECUTION
 # set up pymongo objects
-subjectsCollection,classifCollection,nSj,nCl = pymongo_connection_open()
-# # check if dataframe with classifications exists; if not, run over each classification and store its properties in a pandas dataframe. If it does exist, load it.
-# if os.path.isfile(classificationsDataframeFn) == False:
-#     scores = classifications_dataframe_fill_and_save_one_by_one(maxClassifications=maxClassifications,fn=classificationsDataframeFn)
-# else: #dataframe available, load instead of fill
-#     scores = classifications_dataframe_load(fn=classificationsDataframeFn)
-#
-#
-# # close the connection to local mongoDB
-# pymongo_connection_close()
-# print "All done with script"
+subjectsCollection,classifCollection,dbConnection = pymongo_connection_open()
+# check if dataframe with classifications exists; if not, run over each classification and store its properties in a pandas dataframe. If it does exist, load it.
+if os.path.isfile(classificationsDataframeFn) == False:
+    cl = classifications_dataframe_fill_and_save(fn=classificationsDataframeFn)
+else: #dataframe available, load instead of fill
+    cl = classifications_dataframe_load(fn=classificationsDataframeFn)
+
+# close the connection to local mongoDB
+pymongo_connection_close()
+print "All done with script"
