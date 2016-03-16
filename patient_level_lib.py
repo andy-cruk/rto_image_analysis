49d011d6b7faaf7663e8760f19efd1125ce16577
Peter Smittenaar <petersmittenaar+github@gmail.com>
2016-03-16 18:02:17 +0000

    fixed saving to excel of patient level data to get rid of merged cells. PCA code not tested yet

--- patient_level_lib.py
+++ patient_level_lib.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 import pandas as pd
 import numpy as np
+from ast import literal_eval
+import matplotlib.pyplot as plt
+import sklearn
 
 def get_cores_collection():
     """ Opens a connection to the 'cores' collection in 'results' database through pymongo
@@ -8,7 +11,7 @@ def get_cores_collection():
     """
     db = MongoClient("localhost", 27017)
     coresCollection = db.results.cores
-    return (db, coresCollection)
+    return db, coresCollection
 
 
 def get_patients_collection():
@@ -66,6 +69,7 @@ def combine_cores_per_patient(function=np.nanmean, aggregate='ignoring_segments'
                              aggregate+'.aggregateSQSCorrected',
                              aggregate+'.aggregateIntensityCorrected',
                              aggregate+'.aggregatePropCorrected',
+                             aggregate+'.aggregateSQSCorrectedAdditive',
                              aggregate+'.nClassificationsTotal',
                              aggregate+'.expSQS',
                              aggregate+'.expIntensity',
@@ -74,10 +78,12 @@ def combine_cores_per_patient(function=np.nanmean, aggregate='ignoring_segments'
                          columns=['stain'],
                          aggfunc=function
                          )
-    # change all column types to string
+    # name first and second level
     df3.columns.names = ['measure', 'stain']
     # write to excel with multi-index
-    df3.to_excel('results/patient_aggregated.xlsx')
+    df4 = df3.copy()
+    df4.columns = [' '.join(col).strip() for col in df4.columns.values]
+    df4.to_excel('results/patient_aggregated.xlsx')
 
     # WRITE TO MONGODB
     # this is one way to flatten multi-index on columns
@@ -86,10 +92,71 @@ def combine_cores_per_patient(function=np.nanmean, aggregate='ignoring_segments'
     df3.columns = [s.replace(aggregate+'.', '') for s in df3.columns]
     # write to mongodb. If you ever wanted to get this out again, use pd.MultiIndex.from_tuples
     _, coll = get_patients_collection()
-    # coll.delete_many({})  # empty it if necessary
+    coll.delete_many({})  # empty it if necessary
     coll.insert_many(df3.to_dict('records'))
-
     return df3
+
+
+def read_patient_data_from_mongodb(mongoFilter=None, projection=None):
+    """ Reads the patient data from mongodb, puts it in df, and returns it
+
+    :return: pandas df with patients on row, measures on columns. Multi-indexed
+    """
+    if mongoFilter is None:
+        mongoFilter = {}
+    if projection is None:
+        projection = {'_id': False}
+
+    db, _ = get_cores_collection()
+    coll = db.results.patients
+    results = coll.find(filter=mongoFilter, projection=projection)
+    df = pd.DataFrame(list(results))
+    # exclude patient_key and set up multi-index of (variable, stain)
+    tups = [literal_eval(x) for x in df.columns[:-1]]
+    cols = pd.MultiIndex.from_tuples(tups)
+    # set up new df with patient_key as index. You have to put into np array first, otherwise
+    # the existing columns and indices mess things up and you get nans only
+    df2 = pd.DataFrame(data=np.array(df.iloc[:, :-1]), columns=cols, index=df.patient_key)
+
+    # see this link to index at second level (i.e. of stains)
+    # http://pandas.pydata.org/pandas-docs/stable/advanced.html#advanced-xs
+    return df2
+
+
+def plot_patient_level_correlations(measure='aggregateSQSCorrected'):
+    """
+    Plots a heatmap of correlations between different stains, for a requested measure.
+    Useful to see if two stains correlate, and to see if MRE11 results replicated in test MRE11.
+    :param measure: a string describing the level 0 of multi-index in df to use
+    :return:
+    """
+    df = read_patient_data_from_mongodb()
+    sel = df[measure]
+    plt.imshow(sel.corr(), interpolation='nearest', vmin=-1, vmax=1)
+    plt.xticks(range(len(sel.columns)), sel.columns)
+    plt.yticks(range(len(sel.columns)), sel.columns)
+    plt.colorbar()
+    plt.show()
+
+
+def patient_level_pca(measure='aggregateSQSCorrected'):
+    """
+    Takes a measure and all stains, and runs pca on these. Not sure what output will be yet
+    :param measure: string indicating a measure as recorded in combine_cores_per_patient()
+    :return:
+    """
+    # get df with nPatients*nStains
+    df = combine_cores_per_patient()[measure]
+    # exclude test mre11
+    del df['test mre11']
+    # set up imputer for missing values
+    imp = sklearn.preprocessing.Imputer(missing_values='NaN', strategy='mean', axis=0, verbose=0, copy=True)
+    # set up pca
+    pca = sklearn.decomposition.PCA(n_components=3, copy=True, whiten=False)
+    # run PCA
+    pca.fit(imp.fit(df))
+    print pca.explained_variance_ratio, pca.components, pca.mean,
+
 
 
 
