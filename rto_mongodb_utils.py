39450029444d7f44179bd148fa3f8d88da11b4f8
Peter Smittenaar <petersmittenaar+github@gmail.com>
2016-04-25 06:42:29 -0700

    minor fixes

--- rto_mongodb_utils.py
+++ rto_mongodb_utils.py
@@ -12,11 +12,7 @@ import re
 
 
 # set the name of the database on your local host to connect to
-<<<<<<< HEAD
 currentDB = 'RTO_20160425'
-=======
-# currentDB = 'RTO_20160329'
-currentDB = 'RTO_20160212'
 
 def add_indices(classifCollection,subjectsCollection):
     """ adds indices that will speed up various functions in user_aggregation
