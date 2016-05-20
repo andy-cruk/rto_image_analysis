c47e5830e8b00e9a8ea8c4ca988cd5b3e3dc378d
Peter Smittenaar <peter@petersmittenaar.com>
2016-05-20 09:27:00 +0100

    updated settings

--- core_level_plots.py
+++ core_level_plots.py
@@ -270,11 +270,11 @@ def create_table_summary_stats_each_stain(aggregate=aggregate):
 
 
 if __name__ == "__main__":
-    f, ax = plot_contribution_patterns()
+    # f, ax = plot_contribution_patterns()
     # f, ax = scatter_for_each_stain()
     # r, ci, f, ax = scatter_performance_single_graph()
-    # f, ax = plot_number_of_classifications_against_performance_for_multiple_stains_in_single_graph(
-    #   nUsersPerSubject=np.array([1,2,4,8,16,32,64,128,256,512,1024]))
+    f, ax = plot_number_of_classifications_against_performance_for_multiple_stains_in_single_graph(
+      nUsersPerSubject=np.array([1,2,4,8,16,32,64,128,256,512,1024]))
     # create_table_summary_stats_each_stain()
 
     print "done with core_level_plots.py"
